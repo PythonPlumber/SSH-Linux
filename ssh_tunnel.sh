dc870bfb83368c2dbd02827ba365bbac646d78af
Mario Python Plumber <marioyellowbeard@aol.com>
2023-06-24 12:53:20 +0530

    Update ssh_tunnel.sh

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -7,7 +7,7 @@ LOG_FILE="ssh_tunnel.log"
 # Read SSH configuration from JSON file
 read_config() {
   local profile_name=$1
-  jq -r --arg profile "$profile_name" '.profiles[] | select(.name == $profile)' "$CONFIG_FILE"
+  jq -r --arg profile "$profile_name" '.profiles[] | select(.name == $profile) | {host, username, password, port}' "$CONFIG_FILE"
 }
 
 # Start SSH tunnel
@@ -21,24 +21,19 @@ start_tunnel() {
   fi
 
   local host=$(jq -r '.host' <<< "$config")
-  local port=$(jq -r '.port' <<< "$config")
   local username=$(jq -r '.username' <<< "$config")
-  local identity_file=$(jq -r '.identity_file' <<< "$config")
   local password=$(jq -r '.password' <<< "$config")
-  local local_ports=$(jq -r '.local_ports | join(",")' <<< "$config")
-  local remote_ports=$(jq -r '.remote_ports | join(",")' <<< "$config")
+  local port=$(jq -r '.port' <<< "$config")
 
   log "Starting SSH tunnel for profile: $profile_name"
 
-  if [ -n "$identity_file" ]; then
-    autossh -M 0 -f -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "ExitOnForwardFailure yes" \
-      -i "$identity_file" -L "$local_ports:localhost:$remote_ports" -p "$port" "$username@$host" >> "$LOG_FILE" 2>&1
-  else
-    autossh -M 0 -f -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "ExitOnForwardFailure yes" \
-      -L "$local_ports:localhost:$remote_ports" -p "$port" "$username@$host" >> "$LOG_FILE" 2>&1
-  fi
+  {
+    echo "◈──────────────────◈"
+    echo "©️ 🕸 Spider SSH 🕸"
+    echo "◈──────────────────◈"
+  } | sshpass -p "$password" ssh -N -f -L 8080:localhost:80 -p "$port" "$username@$host" >> "$LOG_FILE" 2>&1
 
-  log "SSH tunnel started for profile: $profile_name. Local ports: $local_ports, Remote ports: $remote_ports"
+  log "SSH tunnel started for profile: $profile_name"
 }
 
 # Stop SSH tunnel
@@ -52,13 +47,12 @@ stop_tunnel() {
   fi
 
   local host=$(jq -r '.host' <<< "$config")
-  local port=$(jq -r '.port' <<< "$config")
   local username=$(jq -r '.username' <<< "$config")
+  local port=$(jq -r '.port' <<< "$config")
 
   log "Stopping SSH tunnel for profile: $profile_name"
 
-  autossh -M 0 -f -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "ExitOnForwardFailure yes" \
-    -O "exit" -p "$port" "$username@$host" >> "$LOG_FILE" 2>&1
+  sshpass -p "$password" ssh -O "exit" -p "$port" "$username@$host" >> "$LOG_FILE" 2>&1
 
   log "SSH tunnel stopped for profile: $profile_name"
 }
@@ -75,11 +69,6 @@ log() {
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "SSH configuration file not found: $CONFIG_FILE"
   exit 1
-fi
-
-# Check if the log file exists, create it if not
-if [ ! -f "$LOG_FILE" ]; then
-  touch "$LOG_FILE"
 fi
 
 # Check the number of arguments
